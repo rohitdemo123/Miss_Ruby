1d6f3d5d8362d61308697de4dc0f786158daa563
Rogerxt9k <81235122+Rogerxt9k@users.noreply.github.com>
2023-12-05 19:35:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://vjfilterbots.onrender.com/".format(FQDN)
+    URL = "".format(FQDN)
 else:
-    URL = "https://vjfilterbots.onrender.com/".format(FQDN)
+    URL = "".format(FQDN)
 
 
 
